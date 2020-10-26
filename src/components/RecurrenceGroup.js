ab5c0f93c06f45619fde5e3834308463cdb5be3a
Jay Quigley <quigley.james.g@gmail.com>
2020-10-26 12:57:34 -0700

    Add Material UI for accordion

--- src/components/RecurrenceGroup.js
+++ src/components/RecurrenceGroup.js
@@ -1,18 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 import LinkSubscriptions from './LinkSubscriptions';
 import RecurrenceRules from './RecurrenceRules';
 import RecurrenceGroupName from './RecurrenceGroupName';
 
+import './RecurrenceGroup.css'
+
 function RecurrenceGroup({ userId, recurrenceGroup }) {
   return (
     <div key={recurrenceGroup.id} className='RecurrenceGroup'>
-      <RecurrenceGroupName recurrenceGroup={recurrenceGroup} />
-      <div>
-        <RecurrenceRules userId={userId} recurrenceGroup={recurrenceGroup} />
-        <LinkSubscriptions userId={userId} recurrenceGroup={recurrenceGroup} />
-      </div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <div className='recurrenceGroupName'>
+            <RecurrenceGroupName recurrenceGroup={recurrenceGroup} />
+          </div>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div>
+            <RecurrenceRules userId={userId} recurrenceGroup={recurrenceGroup} />
+            <LinkSubscriptions userId={userId} recurrenceGroup={recurrenceGroup} />
+          </div>
+        </AccordionDetails>
+      </Accordion>
     </div>
   )
 }
